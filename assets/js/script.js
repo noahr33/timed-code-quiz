ce50b4ca4dfa325df9125e150f1bd20a129f8a7c
Noah <noahkoleske@icloud.com>
2023-06-20 22:42:42 -0500

    added first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,37 @@
 var timeLeft = document.querySelector(".timer")
 var secondsLeft = 60
 
+var answrOne = document.getElementById('answr-1')
+var answrTwo = document.getElementById('answr-2')
+var answrThree = document.getElementById('answr-3')
+var answrFour = document.getElementById('answr-4')
+
+
+var questionOne = {
+    input1: "Red",
+    input2: "Blue",
+    input3: "Green",
+    input4: "Purple"
+}
+
+
+
+function  firstQuestion() {
+
+    var questionOne = {
+        input1: "Red",
+        input2: "Blue",
+        input3: "Green",
+        input4: "Purple"
+    }
+
+    answrOne.textContent = questionOne.input1
+    answrTwo.textContent = questionOne.input2
+    answrThree.textContent = questionOne.input3
+    answrFour.textContent = questionOne.input4
+}
+
+
 function startQuiz () {
     document.getElementById('start-quiz').style.display = "none";
     return questions () , timer ()
@@ -25,6 +56,15 @@ function timer () {
 
 function questions () {
     document.getElementById('questions').style.display = "block";
+
+    firstQuestion()
+
+    
+
+    // for (var i = 0; i < ; i++) {
+        
+    
+    // }
 }
 
 document.getElementById("start-btn").addEventListener('click', startQuiz)
@@ -39,6 +79,8 @@ document.getElementById("start-btn").addEventListener('click', startQuiz)
 // THEN the game is over
 // WHEN the game is over
 function endQuiz () {
-    
+
 }
 // THEN I can save my initials and my score
+
+// window local storage!!!
